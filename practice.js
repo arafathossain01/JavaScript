5f5d37e7b4ef878de45a0cd1194ad99c9abf4b56
Arafat Hossain <arafathossain.cs@gmail.com>
2025-12-01 00:34:31 +0600

    practice.js

--- practice.js
+++ practice.js
@@ -20,10 +20,15 @@ console.log(text);
 // interview question
 console.log(0.1 + 0.2 === 0.3); // false. 0.1 + 0.2 = 0.30000000000000004
 
+let a = "25";
+let b = "234.32px";
+console.log(Number(a));
+console.log(parseInt(b)); // convert string to number with int
+console.log(parseFloat(b)); // convert string to numvber with floating value.
 
+console.log((12.32).toPrecision(3)); // to define total digit
 
-
-
-
-
+let random = Math.random()*100;
+random = Number(random.toFixed(0)); // Number() function convert the string 
+console.log(random);
 
