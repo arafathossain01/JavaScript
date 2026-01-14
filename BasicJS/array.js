48dd2fbef3b5da18a122bbc08a7154a67073247e
Arafat Hossain <arafathossain.cs@gmail.com>
2026-01-15 01:16:11 +0600

    .

---
+++ BasicJS/array.js
@@ -0,0 +1,20 @@
+const arr = [10,5,90,70,85,8,1];
+
+// console.log(arr);
+console.log(arr.sort());
+
+// for (let i = 0; i < 7; i++) {
+//   console.log(arr[i]);
+// }
+
+const carName = ["volvo", "hino", "truck", "premio", "allion"];
+// console.log(carName.toString());
+
+carName.push('Axio');
+console.log(carName); //at last index
+
+console.log(Array.isArray(carName)); // check array or not
+console.log(carName instanceof Array); // check array or not
+
+
+
